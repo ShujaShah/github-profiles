c5672b77e94d9a3dd45415a37d32c2ff4855b212
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-22 12:10:59 +0530

    added a spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -28,7 +29,7 @@ function UserResults() {
       </div>
     );
   } else {
-    return <h3>Loading...</h3>;
+    return <Spinner />;
   }
 }
 
