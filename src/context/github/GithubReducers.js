22c7b54b3842f65f646a3addcb00f0a070fcf601
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-25 22:52:32 +0530

    comments

--- src/context/github/GithubReducers.js
+++ src/context/github/GithubReducers.js
@@ -1,17 +1,17 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
-    case "GET_USERS":
+    case "GET_USERS": // to get the users
       return {
         ...state,
         users: action.payload,
         loading: false,
       };
-    case "SET_LOADING":
+    case "SET_LOADING": // to set the loading spinner
       return {
         ...state,
         loading: true,
       };
-    case "CLEAR_USERS":
+    case "CLEAR_USERS": // clear the users after getting the users
       return {
         ...state,
         users: [],
