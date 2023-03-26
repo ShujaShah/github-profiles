8f30bc37bd80275d01769b1599b59cacd04725d9
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-26 13:32:02 +0530

    alert component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
 
+  const { setAlert } = useContext(AlertContext);
+
   const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text === "") {
-      alert("Please enter something");
+      // alert("Please enter something");
+      setAlert("Please enter something", "error");
     } else {
       searchUsers(text);
 
@@ -40,7 +44,9 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={clearUsers} class="btn btn-ghost btn-large">Clear</button>
+          <button onClick={clearUsers} class="btn btn-ghost btn-large">
+            Clear
+          </button>
         </div>
       )}
     </div>
